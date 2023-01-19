ebfaca380819b515112908dad3f313dcbbfd9595
Richard <ricky.cary1@gmail.com>
2023-01-18 18:12:38 -0600

    Updated Show page w/ form

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, Form } from "react-router-dom"
 
 function Show(props){
 	const book = useLoaderData()
@@ -7,6 +7,13 @@ function Show(props){
 		<div className="book">
 			<h1>{book.website}</h1>
 			<h2>{book.url}</h2>
+
+		
+		<h2>Update {book.website}</h2>
+			<Form action={`/update/${book._id}`} method="post">
+        		<input type="input" name="website" placeholder="Website Name" />
+        		<input type="input" name="url" placeholder="URL" />
+			</Form>
 		</div>
 	)
 }
