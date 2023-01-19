daf6afeed85f5c36932d168cf49224abf47c746d
Richard <ricky.cary1@gmail.com>
2023-01-18 18:30:26 -0600

    Updated Show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,13 +6,13 @@ function Show(props){
 	return(
 		<div className="book">
 			<h1>{book.website}</h1>
-			<h2>{book.url}</h2>
-
+			<h2><a href={book.url}>{book.url}</a></h2>
 		
 		<h2>Update {book.website}</h2>
 			<Form action={`/update/${book._id}`} method="post">
         		<input type="input" name="website" placeholder="Website Name" />
         		<input type="input" name="url" placeholder="URL" />
+				<input type="submit" value={`Update ${book.website}`} /> 
 			</Form>
 		</div>
 	)
